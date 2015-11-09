165c15dee365540ceab7a811b014a81254eb0b64
sergey@telminov.ru <sergey@telminov.ru>
2015-11-09 19:59:35 +0300

    disk space checking and refactor ansible checking

--- parkworker2/monits/general.py
+++ parkworker2/monits/general.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import re
 import os
 import tempfile
 
@@ -11,7 +12,37 @@ from parkworker2 import settings
 from swutils.encrypt import decrypt
 
 
+class ParseStdoutException(Exception):
+    pass
+
+
 class AnsibleMonitMixin(object):
+
+    def run(self, host, remote_user, remote_pass, inventory, kwargs):
+        raise NotImplemented()
+
+    def check(self, host, **kwargs):
+        remote_user, remote_pass = self._get_user_pass(kwargs)
+        inventory = self._create_inventory(host)
+
+        result = self.run(host, remote_user, remote_pass, inventory, kwargs)
+
+        if result['dark'].get(host) and result['dark'][host].get('failed'):
+            return CheckResult(
+                level=const.LEVEL_FAIL,
+                extra=self._correct_result(result),
+            )
+
+        return self.process_contacted(result, kwargs)
+
+    def process_contacted(self, result, kwargs):
+        level = const.LEVEL_OK
+        check_result = CheckResult(
+            level=level,
+            extra=self._correct_result(result),
+        )
+        return check_result
+
     @staticmethod
     def _create_inventory(host):
         fd, host_list_path = tempfile.mkstemp()
@@ -41,15 +72,16 @@ class AnsibleMonitMixin(object):
             corrected_result[k] = v
         return corrected_result
 
+    @staticmethod
+    def _get_contacted(result):
+        return result['contacted'].values()[0]
+
 
 class PingMonit(AnsibleMonitMixin, Monit):
     name = 'ansible.ping'
     description = 'Ansible ping host checking.'
 
-    def check(self, host, **kwargs):
-        remote_user, remote_pass = self._get_user_pass(kwargs)
-        inventory = self._create_inventory(host)
-
+    def run(self, host, remote_user, remote_pass, inventory, kwargs):
         runner = ansible.runner.Runner(
             module_name='ping',
             module_args='',
@@ -58,16 +90,103 @@ class PingMonit(AnsibleMonitMixin, Monit):
             remote_user=remote_user, remote_pass=remote_pass
         )
         result = runner.run()
+        return result
 
-        if result['dark'].get(host) and result['dark'][host].get('failed'):
-            level = const.LEVEL_FAIL
-        else:
-            level = const.LEVEL_OK
 
-        check_result = CheckResult(
-            level=level,
-            extra=self._correct_result(result),
+class DiskSpaceMonit(AnsibleMonitMixin, Monit):
+
+    name = 'ansible.disk_space'
+    description = 'Ansible disk space host checking. Options: \n' \
+                  ' - partitions. Optional. Array with partition names for checking. ' \
+                  'For example: ["/", "/var", "/tmp"] \n' \
+                  ' - warning_percent. Optional. Default 80. \n' \
+                  ' - fail_percent. Optional. Default 95.'
+
+    def run(self, host, remote_user, remote_pass, inventory, kwargs):
+        command = 'df -h'
+        runner = ansible.runner.Runner(
+            module_name='shell',
+            module_args=command,
+            inventory=inventory,
+            pattern=host,
+            remote_user=remote_user, remote_pass=remote_pass
         )
-        return check_result
+        result = runner.run()
+        return result
+
+    def process_contacted(self, result, kwargs):
+        partitions = kwargs.get('partitions')
+        warning_percent = int(kwargs.get('warning_percent', 80))
+        fail_percent = int(kwargs.get('fail_percent', 95))
+
+        contacted = self._get_contacted(result)
+        stdout = contacted['stdout']
+        try:
+            space_data = self._parse_space(stdout)
+            extra = self._analyze_spaces(space_data, partitions, warning_percent, fail_percent)
+
+            if extra.get('fails'):
+                level = const.LEVEL_FAIL
+            elif extra.get('warnings'):
+                level = const.LEVEL_WARNING
+            else:
+                level = const.LEVEL_OK
+
+            return CheckResult(
+                level=level,
+                extra=extra,
+            )
+
+        except ParseStdoutException as ex:
+            return CheckResult(
+                level=const.LEVEL_FAIL,
+                extra={
+                    'stdout': stdout,
+                    'error': 'Parsing stdout error',
+                    'description': str(ex),
+                }
+            )
+
+    @staticmethod
+    def _parse_space(stdout):
+        space_data = []
+        pattern = r'^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)$'
+
+        for line in stdout.split('\n')[1:]:
+            mo = re.match(pattern, line)
+            if not mo:
+                raise ParseStdoutException('Parsing line "%s" error with pattern "%s"' % (line, pattern))
+
+            groups = mo.groups()
+            space_data.append({
+                'disk': groups[0],
+                'size': groups[1],
+                'used': groups[2],
+                'used_percent': int(groups[4][:-1]),
+                'available': groups[3],
+                'partition': groups[5],
+            })
+
+        return space_data
+
+    @staticmethod
+    def _analyze_spaces(space_data, partitions, warning_percent, fail_percent):
+        extra = {'spaces': []}
+
+        for item in space_data:
+            if partitions and item['partition'] not in partitions:
+                continue
+
+            used_percent = item['used_percent']
+            if warning_percent <= used_percent:
+                partition_data = {'partition': item['partition'], 'used_percent': used_percent}
+                if warning_percent <= used_percent < fail_percent:
+                    extra.setdefault('warnings', []).append(partition_data)
+                elif fail_percent < used_percent:
+                    extra.setdefault('fails', []).append(partition_data)
+
+            extra['spaces'].append(item)
+
+        return extra
 
 
